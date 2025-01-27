1c2bca2c3426ea95c94134681e759e8c1bea9801
Jonathan <jsdiaz19.jd@gmail.com>
2025-01-27 15:26:29 -0500

    add fastApi

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import './Projects.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faAngleLeft, faPlay } from '@fortawesome/free-solid-svg-icons'; 
+import { faAngleRight, faAngleLeft, faPlay, faLink } from '@fortawesome/free-solid-svg-icons'; 
 import { dataContext } from '../../../context/dataContext';
 import { useContext, useRef, forwardRef } from 'react';
 
@@ -21,12 +21,15 @@ const Projects = forwardRef(function Projects(props, ref) {
         }
     }
 
+    const goLink = (link)=> {
+        window.open(link, '__blank_')
+    }
     return ( 
         <div className='projectContainer' ref={ref}>
             <h1 className='header'>PROYECTOS</h1>
             <div className='navigation'>
-                <FontAwesomeIcon icon={faAngleLeft} size='2x' className='btnVNav' onClick={scrollLeft}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faAngleRight} size='2x' className='btnVNav active' onClick={scrollRight}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faAngleLeft} size='2x' className='btnVNav' onClick={scrollLeft}/>
+                <FontAwesomeIcon icon={faAngleRight} size='2x' className='btnVNav active' onClick={scrollRight}/>
             </div>
             <div className='contentProject' ref={carouselRef}>
                 {
@@ -36,16 +39,17 @@ const Projects = forwardRef(function Projects(props, ref) {
                                 <h1>{item.title}</h1>
                                 <img src={`/assets/${item.img}`}/>
                                 <div className='barAction'>
-                                    { item.isPublic && <FontAwesomeIcon icon={faPlay} className='btnAct'></FontAwesomeIcon>}
+                                    { item.isPublic && <FontAwesomeIcon icon={faPlay} className='btnAct'/>}
                                 </div>
                             </div>
 
                             <div className='infoHover'>
                                 <div className='sectionHover izq'>{item.descript } </div>
+                                { item.url!='' &&  <div className='contLink' onClick={()=> goLink(item.url)} > <FontAwesomeIcon icon={faLink}  size='4x'/>  </div> }
                                 <div className='sectionHover der'>
                                     {
                                         item.tools.map((tool, index) => (
-                                            <FontAwesomeIcon key={index} icon={tool[0]} color={tool[1]} size='2x'></FontAwesomeIcon>
+                                            tool[2] ? <FontAwesomeIcon key={index} icon={tool[0]} color={tool[1]} size='2x'/> : tool[0]
                                         ))
                                     }
                                 </div>
